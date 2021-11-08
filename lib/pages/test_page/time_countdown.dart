3edfac73e5e8695b73be59a4114bbbb07809c0bd
tham <thampk@outlook.com>
2021-11-08 23:06:20 +0700

    [Update]time_counter {view,,function}

--- lib/pages/test_page/time_countdown.dart
+++ lib/pages/test_page/time_countdown.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Time_countdown extends StatefulWidget {
@@ -8,10 +10,58 @@ class Time_countdown extends StatefulWidget {
 }
 
 class _Time_countdownState extends State<Time_countdown> {
+  int Counter = 20;
+  late Timer _timer;
+
+  void startTimer() {
+    Counter = 20;
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (Counter > 0) {
+        setState(() {
+          Counter--;
+        });
+      } else {
+        _timer.cancel();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$Counter',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  startTimer();
+                },
+                child: Text('Start timer')),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _timer.cancel();
+                    Counter = 20;
+                  });
+                },
+                child: Text('Pause')),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _timer.cancel();
+                    Counter = 20;
+                  });
+                },
+                child: Text('Reset')),
+          ],
+        ),
+      ),
     );
   }
 }
