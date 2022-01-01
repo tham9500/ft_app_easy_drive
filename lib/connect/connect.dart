9e4c7a20a065a34cc1cc538e0b3f64d98d54acb0
tham <thampk@outlook.com>
2022-01-01 23:59:47 +0700

    [UPDATE]play video

--- lib/connect/connect.dart
+++ lib/connect/connect.dart
@@ -1,5 +1,5 @@
 class Domain_name {
-  // String domain = "http://10.0.2.2";
+  //String domain = "http://10.0.2.2";
   String domain = "http://172.27.7.226"; //local tham
   // String domain = "http://10.0.2.2"; // local Gina
   // String domain = "http://10.0.2.2";//local Aum
--- lib/controller/article_test/video_car.dart
+++ lib/controller/article_test/video_car.dart
@@ -1,9 +1,12 @@
+import 'package:ft_app_easy_drive/connect/connect.dart';
+
 class Video_car {
   var car = [
     {
       "ID": "1",
       "text": "car 1",
-      "clip_url": "https://www.youtube.com/watch?v=XJmS_EzZ-nI"
+      "clip_url":
+          "${Domain_name().domain}/easy_drive_backend/video/1640790649.mp4"
     }
   ];
   Video_car();
