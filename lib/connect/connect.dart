caa96651697dd65692e24d32ec6fcb2dc046041c
tham <thampk@outlook.com>
2022-01-03 21:37:48 +0700

    [ADD]forword url in file connect [TEST]video in url DB

--- lib/connect/connect.dart
+++ lib/connect/connect.dart
@@ -1,8 +1,12 @@
 class Domain_name {
   //String domain = "http://10.0.2.2";
-  String domain = "http://172.27.7.226"; //local tham
+  // String domain = "http://172.27.7.226"; //local tham
   // String domain = "http://10.0.2.2"; // local Gina
   // String domain = "http://10.0.2.2";//local Aum
   // String domain = //global server
+  String domain =
+      "http://0bba-203-158-248-18.ngrok.io"; //use to ngrok forword port
+  String forword_port =
+      "https://0bba-203-158-248-18.ngrok.io"; //use to ngrok forword port
   Domain_name();
 }
